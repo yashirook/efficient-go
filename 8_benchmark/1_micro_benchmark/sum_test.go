9b90f82e1a173bccef6af96614a2f5a54d9157b4
yashirook <yashiro.kentaro@gmail.com>
2024-03-30 00:12:17 +0900

    ページ285
    マイクロベンチマークを実行
    例えば、以下のように実行できる
    
    ```
    go test -run '^$' -bench '^BenchmarkSum$'
    ```

--- 8_benchmark/1_micro_benchmark/sum_test.go
+++ 8_benchmark/1_micro_benchmark/sum_test.go
@@ -1,6 +1,8 @@
-package sum_test
+package sum
 
-import "testing"
+import (
+	"testing"
+)
 
 func BenchmarkSum(b *testing.B) {
 	b.ReportAllocs()
@@ -10,6 +12,6 @@ func BenchmarkSum(b *testing.B) {
 	// 初期化処理の時間をベンチマークに含めたくない場合はタイマーをリセット
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		// todo
+		_, _ = Sum("testdata/test.2M.txt")
 	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yashirook/efficient-go
+
+go 1.22.0
