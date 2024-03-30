8753be2bd72d1facaa9c85b377f0d57baee9ef9c
yashirook <yashiro.kentaro@gmail.com>
2024-03-30 10:27:48 +0900

    P298
    テストとベンチマークを共通化するためのヘルパー関数を呼び出すようにした

--- 8_benchmark/1_micro_benchmark/sum_test.go
+++ 8_benchmark/1_micro_benchmark/sum_test.go
@@ -3,6 +3,7 @@ package sum
 import (
 	"testing"
 
+	"github.com/efficientgo/core/testutil"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -22,5 +23,18 @@ func BenchmarkSum(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, err := Sum("testdata/test.2M.txt")
 		assert.NoError(b, err)
+	}
+}
+func TestBenchSum(t *testing.T) {
+	benchmarkSum(testutil.NewTB(t))
+}
+
+func benchmarkSum(tb testutil.TB) {
+	for i := 0; i < tb.N(); i++ {
+		ret, err := Sum("testdata/test.2M.txt")
+		assert.NoError(tb, err)
+		if !tb.IsBenchmark() {
+			assert.Equal(tb, int64(32773481347), ret)
+		}
 	}
 }
