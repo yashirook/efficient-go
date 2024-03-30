0cd36b005c76dba0664a7febd60cf2c9652869d2
yashirook <yashiro.kentaro@gmail.com>
2024-03-30 11:01:42 +0900

    P300
    テスト用のファイルを動的に生成するようにした

--- 8_benchmark/1_micro_benchmark/sum_test.go
+++ 8_benchmark/1_micro_benchmark/sum_test.go
@@ -1,12 +1,28 @@
 package sum
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/efficientgo/core/errcapture"
 	"github.com/efficientgo/core/testutil"
+	"github.com/efficientgo/examples/pkg/sum/sumtestutil"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 
+// `Sum`関数をベンチマークする。
+// 推奨の実行オプションは以下
+/*
+export ver=v1 && \
+    go test -run '^$' -bench '^BenchmarkSum$' -benchtime 10s -count 6 \
+        -cpu 4 \
+        -benchmem \
+        -memprofile=${ver}.mem.pprof \
+        -cpuprofile=${ver}.cpu.pprof \
+    | tee ${ver}.txt
+*/
 func BenchmarkSum(b *testing.B) {
 	benchmarkSum(testutil.NewTB(b))
 }
@@ -15,11 +31,34 @@ func TestBenchSum(t *testing.T) {
 }
 
 func benchmarkSum(tb testutil.TB) {
+	fn := filepath.Join(tb.TempDir(), "/test.2M.txt")
+	sum, err := createTestInput(fn, 2e6)
+	assert.NoError(tb, err)
+
 	for i := 0; i < tb.N(); i++ {
-		ret, err := Sum("testdata/test.2M.txt")
+		ret, err := Sum(fn)
 		assert.NoError(tb, err)
 		if !tb.IsBenchmark() {
-			assert.Equal(tb, int64(32773481347), ret)
+			assert.Equal(tb, sum, ret)
 		}
 	}
+}
+
+func createTestInput(fn string, numLen int) (sum int64, err error) {
+	return createTestInputWithExpectedResult(fn, numLen)
+}
+
+func createTestInputWithExpectedResult(fn string, numLen int) (sum int64, err error) {
+	err = os.MkdirAll(filepath.Dir(fn), os.ModePerm)
+	if err != nil {
+		return 0, err
+	}
+
+	f, err := os.OpenFile(fn, os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return 0, errors.Wrap(err, "open")
+	}
+	defer errcapture.Do(&err, f.Close, "close file")
+
+	return sumtestutil.CreateTestInputWithExpectedResult(f, numLen)
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.22.0
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/efficientgo/core v1.0.0-rc.2 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/efficientgo/examples v0.0.0-20240107094241-3b473a0075bd // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
