db04d64b20ca690e4ab85f5f1894d725f70d8d4d
yashirook <yashiro.kentaro@gmail.com>
2024-03-30 10:29:03 +0900

    P298
    ベンチマークからもヘルパー関数を利用するようにした

--- 8_benchmark/1_micro_benchmark/sum_test.go
+++ 8_benchmark/1_micro_benchmark/sum_test.go
@@ -14,16 +14,7 @@ func TestSum(t *testing.T) {
 }
 
 func BenchmarkSum(b *testing.B) {
-	b.ReportAllocs()
-
-	// 【TODO】初期化処理がある場合は追加する
-
-	// 初期化処理の時間をベンチマークに含めたくない場合はタイマーをリセット
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_, err := Sum("testdata/test.2M.txt")
-		assert.NoError(b, err)
-	}
+	benchmarkSum(testutil.NewTB(b))
 }
 func TestBenchSum(t *testing.T) {
 	benchmarkSum(testutil.NewTB(t))
